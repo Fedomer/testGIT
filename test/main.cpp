98cef240be712efeca4f766a72223463026e6f21
Emiliano Pallotti <emilianopallotti@macbookmemy.local>
2016-10-11 22:38:33 +0200

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Emiliano Pallotti on 11/10/16.
+//  Copyright © 2016 Emiliano Pallotti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
